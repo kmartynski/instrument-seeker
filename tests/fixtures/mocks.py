4ff309a09946e0ad68bafd3f59e307b94999ed90
Karol Martyński <k.martynski@ymail.com>
2022-07-30 22:57:56 +0200

    Improvements

---
+++ tests/fixtures/mocks.py
@@ -0,0 +1,26 @@
+RESULTS_FOR_PIANO = {
+    'YAMAHA CP-60M - 0':
+        [
+            'Open Box',
+            '$2,946.79',
+            'https://www.ebay.com/itm/295094572198?hash=item44b501d4a6:g:txUAAOSwOptiDQ6b'
+        ],
+    'YAMAHA CP-60M - 1':
+        [
+            'Open Box',
+            '$2,946.79',
+            'https://www.ebay.com/itm/165579358615?hash=item268d4c8197:g:hI8AAOSwJm5it8op'
+        ],
+    'YAMAHA CP-60M - 2':
+        [
+            'Open Box',
+            '$2,946.79',
+            'https://www.ebay.com/itm/165595980405?hash=item268e4a2275:g:mlsAAOSw2ENi31Wv'
+        ],
+    'YAMAHA CP-60M - 3':
+        [
+            'Open Box',
+            '$2,946.79',
+            'https://www.ebay.com/itm/164687506552?hash=item265823ec78:g:mrEAAOSw38hgHYY7'
+        ]
+}
--- tests/test_piano_finder.py
+++ tests/test_piano_finder.py
@@ -1,13 +1,23 @@
+from httpx import Response
+import respx
 from unittest.mock import MagicMock, patch
 
-from piano_seeker import PianoFinder
-
+from enums import Pianos
 from main import main
+from piano_seeker import PianoFinder, PIANO_PAGES_MAPPING
+from tests.fixtures.mocks import RESULTS_FOR_PIANO
 
 
 class TestPianoFinder:
 
     @patch.object(PianoFinder, "get_offers")
-    def test_piano_finder_returns_offers(self, finder_mock: MagicMock):
+    def test_piano_finder_returns_number_of_offers(self, finder_mock: MagicMock):
         main()
         assert finder_mock.call_count == 5
+
+    def test_returns_offer(self, respx_mock: respx.MockRouter):
+        respx_mock.get(
+            PIANO_PAGES_MAPPING[Pianos.YAMAHA_CP_60_M.value]
+        ).mock(return_value=Response(200, text=str(RESULTS_FOR_PIANO)))
+        response = PianoFinder(Pianos.YAMAHA_CP_60_M.value).get_offers()
+        assert response == RESULTS_FOR_PIANO
